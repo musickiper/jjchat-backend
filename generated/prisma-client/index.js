70505ab8bd7193d5cc799f1af60fa671f5b7f3b5
James Jung <leedo01219@gmail.com>
2019-12-17 14:13:30 -0500

    Setted up Prisma ORM

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://jjchat-prisma-6a08ff6ac6.herokuapp.com/jjchat-backend/dev`
+});
+exports.prisma = new exports.Prisma();
