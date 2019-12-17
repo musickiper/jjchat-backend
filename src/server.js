4cbb79479b6a096c9c32cfb6f8a1ae6f2f64f9c5
James Jung <leedo01219@gmail.com>
2019-12-17 13:13:21 -0500

    Setted up basic app structure

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import "./env";
+import { GraphQLServer } from "graphql-yoga";
+import schema from "./schema";
+import logger from "morgan";
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger("dev"));
+
+server.start({ port: PORT }, () => console.log(`Server in on port ${PORT}`));
