eb667c300a5b1427203ca536c356d09ad7be9800
James Jung <leedo01219@gmail.com>
2020-01-01 21:55:39 -0500

    Changed filter logic to check room existence to prevent exceptional cases

--- src/api/Message/sendMessage/sendMessage.js
+++ src/api/Message/sendMessage/sendMessage.js
@@ -1,69 +1,25 @@
-import { prisma } from "../../../../generated/prisma-client";
+import {prisma} from "../../../../generated/prisma-client";
 
 export default {
-  Mutation: {
-    sendMessage: async (_, args, { request, isAuthenticated }) => {
-      isAuthenticated(request);
-      const { user } = request;
-      const { toIds, text } = args;
-      try {
-        const roomExistence = await prisma.$exists.room({
-          participants_every: {
-            id_in: [user.id, ...toIds]
-          },
-          participants_none: {
-            id_not_in: [user.id, ...toIds]
-          }
-        });
-        if (!roomExistence) {
-          const participants = toIds.map(toId => ({ id: toId }));
-          const newRoom = await prisma.createRoom({
-            participants: {
-              connect: [{ id: user.id }, ...participants]
-            }
-          });
-          return prisma.createMessage({
-            text,
-            user: {
-              connect: {
-                id: user.id
-              }
-            },
-            room: {
-              connect: {
-                id: newRoom.id
-              }
-            }
-          });
-        } else {
-          const room = await prisma.rooms({
-            where: {
-              participants_every: {
-                id_in: [user.id, ...toIds]
-              },
-              participants_none: {
-                id_not_in: [user.id, ...toIds]
-              }
-            }
-          });
+    Mutation: {
+        sendMessage: async (_, args, {request, isAuthenticated}) => {
+            isAuthenticated(request);
+            const {user} = request;
+            const {roomId, text} = args;
 
-          return prisma.createMessage({
-            text,
-            user: {
-              connect: {
-                id: user.id
-              }
-            },
-            room: {
-              connect: {
-                id: room[0].id
-              }
-            }
-          });
+            return prisma.createMessage({
+                text,
+                room: {
+                    connect: {
+                        id: roomId
+                    }
+                },
+                user: {
+                    connect: {
+                        id: user.id
+                    }
+                }
+            });
         }
-      } catch (e) {
-        throw Error(e);
-      }
     }
-  }
 };
